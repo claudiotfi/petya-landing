f8536207b3ca0d3aeed35d1d5069bbc025da9d45
Claudio <claudiotf@gmail.com>
2025-12-23 14:25:28 -0300

    footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <footer className="py-8 text-center text-slate-400 bg-slate-900">
-      © 2026 Petya — Saúde do seu pet, com cuidado e organização
+      © 2026 Petya — Histórico de Saúde do seu Pet, com cuidado e organização
     </footer>
   )
 }
